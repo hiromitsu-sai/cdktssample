10c758a02b6b4ecc9c949c733f83361ca3ac70cf
swx-sai <124345558+hiromitsu-sai@users.noreply.github.com>
2025-04-01 02:56:25 +0000

    first commit

--- src/lambda/foo.py
+++ src/lambda/foo.py
@@ -1,11 +1,8 @@
 from lib.log import get_logger
-from settings import settings
 
 logger = get_logger(__name__)
 
 
 def handler(event, context):
-    """フォローバックする"""
     logger.info("event: %s", event)
-    return { "message": "samplemsg" , statusCode: 200 }
-
+    return {"Message": "samplemsg", "StatusCode": 200}
